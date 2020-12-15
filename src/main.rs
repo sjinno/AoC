dc14a2be85585ddbc808418682a9aab09fe33589
Shohei Jinno <shohei.jinno@gmail.com>
2020-12-14 22:43:29 -0800

    Initialize problem Day 2.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-mod day1;
+// mod day1;
+mod day2;
 
 fn main() {
-    if let Err(e) = day1::part_1() {
-        println!("Error occured: {}.", e.to_string());
-    }
-    if let Err(e) = day1::part_2() {
+    // // DAY 1
+    // if let Err(e) = day1::part_1() {
+    //     println!("Error occured: {}.", e.to_string());
+    // }
+    // if let Err(e) = day1::part_2() {
+    //     println!("Error occured: {}.", e.to_string());
+    // }
+
+    // DAY 2
+    if let Err(e) = day2::password_philosophy() {
         println!("Error occured: {}.", e.to_string());
     }
 }
